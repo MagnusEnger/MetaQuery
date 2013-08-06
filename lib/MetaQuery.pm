d4817f30b3ad9a42fc94646141febe77c00c88d1
Magnus Enger <magnus@enger.priv.no>
2013-08-06 13:31:25 +0200

    Add more comments

--- lib/MetaQuery.pm
+++ lib/MetaQuery.pm
@@ -15,9 +15,11 @@ get '/**' => sub {
     my $uri = request->uri_base . request->uri;
     $uri =~ s/localhost:3000/metaquery.libriotech.no/;
     
-    ##  First, get all the queries and templates based on the type of the URI
+    ##  First, get all the queries and templates based on the type of the given URI
     
     # Get the type query
+    # $queries will be an iterator that holds a list of queries, each of which
+    # is made up of a slug, a query and a template
     my $typesparql = _get_typesparql( $uri );
     debug $typesparql;
     my $typequery = RDF::Query::Client->new( $typesparql );
@@ -26,6 +28,9 @@ get '/**' => sub {
     # Simplify the datastructure in $queries a bit
     # This will give us a hash of hashes, with the slugs as keys, 
     # and two keys in the inner hash: sparql and template
+    # (It looked like the DISTINCT part of the type query did not
+    # work at some point, this operation will have the side effect 
+    # of ensuring we are working on unique slugs.)
     my %datapoints;
     while (my $row = $queries->next) {
         my $slug = $row->{slug}->literal_value;
@@ -35,33 +40,43 @@ get '/**' => sub {
         };
     }
 
-
+    ## Get the main template, based on the type of the given URI
+    
     my $templatesparql = _get_templatesparql( $uri );
     my $templatequery = RDF::Query::Client->new( $templatesparql );
     my $templateiterator = $templatequery->execute( $endpoint );
+    # Grab the first template (there should not be more than one)
     my $t = $templateiterator->next;
     my $template = $t->{template}->literal_value;
     debug '*** Template: ' . $template;
     
+    ## Iterate over the queries, and collect their data
+    
     foreach my $key ( keys %datapoints ) {
+        
+        # Get the SPARQL query
         my $sparql = $datapoints{ $key }{ 'sparql' };
+        
+        # Insert the given URI into the query
         $sparql =~ s/__URI__/$uri/;
         debug '*** SPARQL: ' . $sparql;
+        
+        # Run the query
         my $q = RDF::Query::Client->new( $sparql );
         my @querydata = $q->execute( $endpoint );
-        # if ( @querydata ) {
         debug '*** Data: ' . Dumper @querydata;
+        # Add the data to %datapoints with a new key called "data"
         $datapoints{ $key }{ 'data' } = \@querydata;
-        # } else {
-        #     debug "http_response: " . Dumper $q->http_response;
-        #     debug "error: " .         $q->error;
-        # }
         
     }
+    
+    ## Process the template, with %datapoints as input
     
     template 'index', { t => $template, d => \%datapoints };
 };
 
+# Construct a query that collects all relevant queries, 
+# based on the type of the given URI
 sub _get_typesparql {
 
     my $uri = shift;
@@ -76,6 +91,8 @@ return "SELECT DISTINCT ?slug ?sparql ?template WHERE {
 
 }
 
+# Construct a query that gets the "main" template, based
+# on the type of the given URI
 sub _get_templatesparql {
 
     my $uri = shift;
